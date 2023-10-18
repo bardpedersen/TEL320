dd1a220ab221aae258d89c184e1dd24d36e4ad25
PederBuk <peder.ormen.bukaasen.email@nmbu.no>
2023-10-18 10:47:34 +0200

    added peak detection files

---
+++ A111/Peak_detection/peak_detection.c
@@ -0,0 +1,55 @@
+/*
+ * peak_detection.c
+ *
+ *  Created on: Oct 11, 2023
+ *      Author: pbuka
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+
+struct Tuple{
+	int index;
+	uint16_t value;
+};
+int* mutliple_peak_detection(uint16_t *data, uint16_t data_length){
+
+	uint16_t sum = 0;
+	for (int i = 0; i < data_length; i++){
+		sum = sum + data[i];
+
+	}
+	uint16_t baseline = sum/data_length;
+	struct Tuple peaks[] = {
+						   {0,0},
+						   {0,0}
+	};
+
+	for (int i = 0;i<data_length;i++){
+		if (data[i] > baseline && data[i] > data[i-1]){
+			if (data[i] > peaks[0].value){
+				peaks[0].index = i;
+				peaks[0].value = data[i];
+
+			}else if (data[i] > peaks[1].value){
+				peaks[0].index = i;
+				peaks[0].value = data[i];
+			}
+		}
+	}
+	int* peakIndexes = (int*)malloc(2 * sizeof(int));
+
+	peakIndexes[0] = peaks[0].index;
+	peakIndexes[1] = peaks[1].index;
+
+	return peakIndexes;
+
+
+
+}
+
+
---
+++ A111/Peak_detection/peak_detection.h
@@ -0,0 +1,16 @@
+/*
+ * peak_detection.h
+ *
+ *  Created on: Oct 11, 2023
+ *      Author: pbuka
+ */
+#include <stdio.h>
+
+#ifndef SRC_PEAK_DETECTION_H_
+#define SRC_PEAK_DETECTION_H_
+
+
+int* mutliple_peak_detection(uint16_t *data, uint16_t data_length);
+
+
+#endif /* SRC_PEAK_DETECTION_H_ */
